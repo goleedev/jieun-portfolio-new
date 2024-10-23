a9511315c1adb653c7c314781bde3e038dda2a54
GO Lee <golee.dev@gmail.com>
2024-10-24 00:10:34 +0100

    refactor: update Header component styling and navigation logic

--- components/header.tsx
+++ components/header.tsx
@@ -5,24 +5,35 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useMemo, useState } from 'react';
 
+import { cn } from '@/utils/merge-classes';
+import { CloseIcon, MoreIcon } from './assets';
+
 const NavLink = ({
   title,
   href,
   onClick,
   isActive,
+  isLast,
 }: {
   title: string;
   href: string;
   onClick?: () => void;
   isActive: boolean;
+  isLast: boolean;
 }) => (
   <Link
     href={href}
     onClick={onClick}
-    className="group relative font-pretendard font-medium text-4xl md:font-normal md:text-base md:h-10 md:py-[9px] md:px-3.5"
+    className="group relative font-pretendard text-4xl font-normal md:text-base md:h-10 md:py-[9px] md:px-3.5"
   >
-    <p className={`flex ${isActive ? 'italic' : ''}`}>
-      <span>{title}</span>
+    <p
+      className={cn(
+        'text-center',
+        !isLast && 'pb-12',
+        isActive && 'italic pb-12'
+      )}
+    >
+      {title}
     </p>
   </Link>
 );
@@ -42,11 +53,16 @@ export default function Header() {
 
   const navLinks = useMemo(
     () =>
-      navItems.map(({ title, href }) => (
+      navItems.map(({ title, href }, index) => (
         <NavLink
           key={href}
           title={title}
           href={href}
+          isLast={
+            index === navItems.length - 1 ||
+            (href === '/' && pathname === '/') ||
+            (href !== '/' && pathname.startsWith(href))
+          }
           isActive={
             href === pathname || (href !== '/' && pathname.startsWith(href))
           }
@@ -58,44 +74,40 @@ export default function Header() {
 
   return (
     <header className="w-full py-4 h-20">
-      <div className="flex items-center h-10 md:h-auto">
-        {!isMenuOpen && (
-          <Link href="/" className="flex-shrink-0">
-            <Image
-              src="/images/logo.png"
-              alt="Logo"
-              width={65}
-              height={40}
-              className="cursor-pointer w-[65px] h-10"
-              priority
-            />
-          </Link>
-        )}
+      <div
+        className={
+          'flex justify-between md:justify-normal items-center h-10 md:h-auto'
+        }
+      >
+        <Link href="/" className="flex-shrink-0">
+          <Image
+            src={isMenuOpen ? '/images/logo-inverted.png' : '/images/logo.png'}
+            alt="Logo"
+            width={65}
+            height={40}
+            className="cursor-pointer w-[65px] h-10 z-50 fixed left-6 top-5"
+            priority
+          />
+        </Link>
 
-        <nav className="opacity-0 md:opacity-100 md:flex md:w-[calc(100%-130px)] justify-center h-12">
+        <nav className="hidden md:flex w-full justify-center h-12">
           <ul className="flex space-x-1 items-center">{navLinks}</ul>
         </nav>
 
         <button
-          className="md:hidden flex items-center z-50 pr-6"
+          className="md:hidden flex items-center fixed right-6 z-40 w-12 h-12 justify-center"
           onClick={toggleMenu}
           aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
         >
-          <span
-            className={`font-mono text-xs ${
-              isMenuOpen ? 'text-white md:text-black' : ''
-            }`}
-          >
-            {isMenuOpen ? 'CLOSE' : 'MENU'}
-          </span>
+          {isMenuOpen ? <CloseIcon width={24} /> : <MoreIcon width={24} />}
         </button>
       </div>
 
       <>
         {isMenuOpen && (
-          <nav className="md:hidden fixed inset-x-0 top-0 bg-dark-gray z-30">
-            <div className="flex flex-col items-center justify-center h-full relative pt-10">
-              <ul className="space-y-10 text-white">{navLinks}</ul>
+          <nav className="md:hidden fixed min-h-dvh inset-x-0 top-0 left-0 bg-[#222] z-30">
+            <div className="flex flex-col items-center justify-center relative min-h-dvh">
+              <ul className="text-[#BCBDFD]">{navLinks}</ul>
             </div>
           </nav>
         )}
