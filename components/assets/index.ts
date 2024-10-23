5534c46016d38677204dce112cd0bd856c21c76e
GO Lee <golee.dev@gmail.com>
2024-10-24 00:11:04 +0100

    refactor: add SVG icons to assets and update Next.js config

---
+++ components/assets/index.ts
@@ -0,0 +1,5 @@
+'use client';
+
+export { default as MoreIcon } from './svgs/more.svg';
+export { default as CancelIcon } from './svgs/cancel.svg';
+export { default as CloseIcon } from './svgs/close.svg';
