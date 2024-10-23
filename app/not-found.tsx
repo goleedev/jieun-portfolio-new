befa24f993463f34084ac76cd6ed9b756053f9dd
GO Lee <golee.dev@gmail.com>
2024-10-24 00:28:53 +0100

    refactor: simplify NotFound component structure and styling

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,24 +6,22 @@ const currentYear = new Date().getFullYear();
 
 export default function NotFound() {
   return (
-    <section className="w-full flex flex-col items-center justify-center text-center">
-      <div className="min-h-[calc(100vh-156px)] flex flex-col justify-center items-center">
-        <section className="w-full flex items-center justify-center text-center font-mono font-black tracking-tighter leading-none overflow-auto cursor-default">
-          <TextLine>
-            <span>4</span>
-            <ImageComponent src="/gifs/0.gif" alt="0 gif" className="-mx-12" />
-            <span>4</span>
-          </TextLine>
-        </section>
+    <section className="w-full flex flex-col min-h-[calc(100vh-156px)] items-center justify-center text-center">
+      <section className="w-full flex items-center justify-center text-center font-mono font-black tracking-tighter leading-none cursor-default">
+        <TextLine>
+          <span>4</span>
+          <ImageComponent src="/gifs/0.gif" alt="0 gif" className="-mx-12" />
+          <span>4</span>
+        </TextLine>
+      </section>
 
-        <div className="pt-[18px]">
-          <p className="font-medium font-pretendard text-lg leading-10">
-            Not Found
-          </p>
-          <p className="font-pretendard text-lg leading-0">
-            Sorry, that page doesn’t exist!
-          </p>
-        </div>
+      <div className="pt-[18px]">
+        <p className="font-medium font-pretendard text-lg leading-10">
+          Not Found
+        </p>
+        <p className="font-pretendard text-lg leading-0">
+          Sorry, that page doesn’t exist!
+        </p>
       </div>
 
       <footer className="fixed bottom-6 text-center font-pretendard text-lg leading-6 text-[#999]">
