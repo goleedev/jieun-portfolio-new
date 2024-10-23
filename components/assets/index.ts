334e08bbe00b3a4266a96dfe5f3ed9e753fd9df4
GO Lee <golee.dev@gmail.com>
2024-10-24 00:12:24 +0100

    refactor: remove unused CancelIcon import from assets/index.ts

--- components/assets/index.ts
+++ components/assets/index.ts
@@ -1,5 +1,4 @@
 'use client';
 
 export { default as MoreIcon } from './svgs/more.svg';
-export { default as CancelIcon } from './svgs/cancel.svg';
 export { default as CloseIcon } from './svgs/close.svg';
