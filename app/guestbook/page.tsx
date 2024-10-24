95c466162c3af9f9f6f8bdda5835bc55b3b4ab88
GO Lee <golee.dev@gmail.com>
2024-10-24 16:00:02 +0100

    feat: add GuestbookPage component with images and HomeContent

---
+++ app/guestbook/page.tsx
@@ -0,0 +1,34 @@
+import HomeContent from '@/components/home-content';
+import Image from 'next/image';
+
+export default async function GuestbookPage() {
+  return (
+    <>
+      <Image
+        src="/images/image2.png"
+        alt="smile"
+        width={300}
+        height={300}
+        className="w-[272px] h-[272px] absolute top-[144px] inset-x-1/3 -z-10"
+        unoptimized
+      />
+      <HomeContent />
+      <Image
+        src="/images/image6.png"
+        alt="smile"
+        width={500}
+        height={300}
+        className="-rotate-0 absolute inset-y-2/3 left-20"
+        unoptimized
+      />
+      <Image
+        src="/images/image4.png"
+        alt="smile"
+        width={250}
+        height={350}
+        className="w-[248px] h-[332px] absolute bottom-10 right-[100px]"
+        unoptimized
+      />
+    </>
+  );
+}
