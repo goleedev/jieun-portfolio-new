e98e52575f723fe345e81bd8321250b5fdacc0b9
GO Lee <golee.dev@gmail.com>
2024-10-29 15:44:27 +0000

    refactor: update home-content component to improve alignment and add guestbook message

--- app/guestbook/components/home-content.tsx
+++ app/guestbook/components/home-content.tsx
@@ -2,7 +2,7 @@ import { ImageComponent, TextLine } from '@/components/home-content';
 
 function HomeContent(): JSX.Element {
   return (
-    <section className="w-full min-h-[calc(100vh-128px)] flex flex-col items-center justify-center text-center font-mono font-black tracking-tighter leading-none overflow-auto cursor-default">
+    <section className="w-full min-h-[calc(100vh-80px)] flex flex-col items-center justify-center text-center font-mono font-black tracking-tighter leading-none overflow-auto cursor-default">
       <div className="flex flex-col items-center">
         <TextLine>
           <span>L</span>
@@ -17,6 +17,11 @@ function HomeContent(): JSX.Element {
           <ImageComponent src="/gifs/E.gif" alt="E gif" />
         </TextLine>
       </div>
+
+      <p className="absolute flex flex-col md:whitespace-nowrap lg:flex-row md:bottom-6 bottom-5 md:text-lg md:font-medium font-normal text-sm leading-5 md:leading-6 text-center">
+        <span>After reviewing my projects, feel free to leave</span>
+        <span>any questions or comments in the guestbook.</span>
+      </p>
     </section>
   );
 }
