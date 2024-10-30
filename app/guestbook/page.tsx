7f47950d888a079ce2fa168b844774f7920bdc90
GO Lee <golee.dev@gmail.com>
2024-10-30 21:23:06 +0000

    refactor: update guestbook page component to improve alignment and responsiveness

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 import getGuestbook from '@/data/get-guestbook';
 import { Tables } from '@/types/database-generated.types';
-import { useEffect, useState } from 'react';
 import GuestbookForm from './components/guestbook-form';
 import GuestbookList from './components/guestbook-list';
 import HomeContent from './components/home-content';
@@ -28,7 +28,7 @@ export default function GuestbookPage() {
         alt="moon"
         width={300}
         height={300}
-        className="w-[272px] h-[272px] absolute top-[144px] inset-x-1/3 -z-10"
+        className="absolute -z-10 moon-responsive"
         unoptimized
       />
       <HomeContent />
@@ -37,22 +37,17 @@ export default function GuestbookPage() {
         alt="pencil"
         width={500}
         height={300}
-        className="absolute inset-y-2/3 left-20"
+        className="absolute pencil-responsive -rotate-[16deg] -z-10 md:z-10"
         unoptimized
       />
       <Image
         src="/images/image4.png"
         alt="gameboy"
-        width={250}
-        height={350}
-        className="w-[248px] h-[332px] absolute bottom-10 right-[100px]"
+        width={330}
+        height={400}
+        className="absolute gameboy-responsive"
         unoptimized
       />
-
-      <p className="text-lg font-medium leading-6 text-center pt-[125px] pb-[104px]">
-        After reviewing my projects, feel free to leave any questions or
-        comments in the guestbook.
-      </p>
 
       <GuestbookForm onMessageSubmitted={fetchMessages} />
       <GuestbookList messages={messages} />
