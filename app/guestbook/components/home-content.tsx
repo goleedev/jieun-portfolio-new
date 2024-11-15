76a3e70b05c2884b14c38630394358378d7ea5df
GO Lee <golee.dev@gmail.com>
2024-11-15 17:19:53 +0000

    feat: update HomeContent component styling; adjust font class for improved readability

--- app/guestbook/components/home-content.tsx
+++ app/guestbook/components/home-content.tsx
@@ -2,8 +2,8 @@ import { ImageComponent, TextLine } from '@/components/home-content';
 
 function HomeContent(): JSX.Element {
   return (
-    <section className="w-full min-h-[calc(100vh-108px)] mb-7 flex flex-col items-center justify-center text-center font-mono font-black tracking-tighter leading-none overflow-auto cursor-default">
-      <div className="flex flex-col items-center">
+    <section className="w-full min-h-[calc(100vh-108px)] mb-7 flex flex-col items-center justify-center text-center font-black tracking-tighter leading-none overflow-auto cursor-default">
+      <div className="flex flex-col items-center font-mono">
         <TextLine>
           <span>L</span>
           <ImageComponent src="/gifs/E.gif" alt="E gif" />
