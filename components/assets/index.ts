4b22b2eec567c5c76a47e34fcdbde6ca35d51089
GO Lee <golee.dev@gmail.com>
2024-11-15 00:36:17 +0000

    feat: add CalendarIcon SVG and update asset exports

--- components/assets/index.ts
+++ components/assets/index.ts
@@ -1,4 +1,5 @@
 'use client';
 
-export { default as MoreIcon } from './svgs/more.svg';
+export { default as CalendarIcon } from './svgs/calendar.svg';
 export { default as CloseIcon } from './svgs/close.svg';
+export { default as MoreIcon } from './svgs/more.svg';
